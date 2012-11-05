ce33c9421d743473a30895b46c17fbe3e36cef6f
Alex Burkhart <saterus@gmail.com>
2012-11-05 16:37:45 -0500

    rails new the-great-game

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+gem 'sqlite3'
+
+gem 'jquery-rails'
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
+group :development do
+  gem 'pry'
+  gem 'pry-remote'
+  gem 'pry-nav'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheGreatGame::Application.config.secret_token = '2123e5b1f81f885859ac4dc98e885b2ab2566806daa2fe1f9160b268e2e5dbf3aa37a3d78e9a19c38227aa4a6a1cadefed995b6f535ceb0eb5982b50ba154656'
